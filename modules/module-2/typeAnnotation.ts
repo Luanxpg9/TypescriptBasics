4fc8113573c5431938fb3118a9942359746680a5
Luan Barbosa <luanxpg9@gmail.com>
2022-09-04 19:26:20 -0300

    Modulo 1 e 2

---
+++ modules/module-2/typeAnnotation.ts
@@ -0,0 +1,205 @@
+//#region Variables
+console.log("---------------------------------");
+console.log("Variáveis\n");
+
+let nome: string = '1';
+console.log(nome);
+
+//#endregion
+
+//#region Arrays
+console.log("---------------------------------");
+console.log("Arrays\n");
+
+let animais: string[] = ['Elefante', 'Gato', 'Cachorro', 'Panda'];
+console.log(animais);
+
+//#endregion
+
+//#region Objects
+console.log("---------------------------------");
+console.log("Objetos\n");
+
+let carro: {
+    nome: string,
+    ano: number,
+    preco: number
+};
+
+carro = {nome: 'Toyota Yaris Sedan XS', ano: 2021, preco: 80000}
+console.log(carro);
+
+//#endregion
+
+//#region Functions
+console.log("---------------------------------");
+console.log("Funções\n");
+
+function multiplicarNumeros(num1: number, num2: number) {
+    return num1 * num2;
+}
+
+console.log(multiplicarNumeros(12, 10));
+
+//#endregion
+
+//#region Boolean
+console.log("---------------------------------");
+console.log("Boolean\n");
+/* Lembrando que Boolean != de boolean
+ * Boolean é um objeto do javascript
+ * é recomendado utilizar o boolean pois é um tipo primitivo.
+*/
+
+let isTarefaConcluida: boolean = Math.random() > 0.5;
+isTarefaConcluida ? console.log('Tarefa concluida com sucesso') : console.log('Tarefa pendente')
+
+// #endregion
+
+//#region Number & Bigint
+console.log("---------------------------------");
+console.log("Number e BigInt\n");
+
+
+let num1: number = 3.141592653589793;   // number
+let num2: number = 0x37CF;          // hex
+let num3: number = 0o377;           // octal
+let num4: number = 0b111001;        // bin
+
+console.log('Float:\t\t' + num1);
+console.log('Hex:\t\t' + num2);
+console.log('Octal:\t\t' + num3);
+console.log('Bin:\t\t' + num4);
+
+/* Funciona apenas para ES2020 ou posterior */
+console.log("---------------------------------");
+let big1: bigint = 9007199254740995n;
+// bin
+let big2: bigint = 0b100000000000000000000000000000000000000000000000000011n;
+// hex
+let big3: bigint = 0x20000000000003n;
+// octal
+let big4: bigint = 0o400000000000000003n;
+
+console.log('Float:\t' + big1);
+console.log('Bin:\t' + big2);
+console.log('Hex:\t' + big3);
+console.log('Octal:\t' + big4);
+
+//#endregion
+
+//#region String
+console.log("---------------------------------");
+console.log("String\n");
+
+let animal: string = 'Elefante';
+console.log(animal);
+
+let primeiroNome: string = 'André'; // Single cotes
+let segundoNome: string = "Fernandes"; // Double cotes
+let nomeCompleto: string = `${primeiroNome} ${segundoNome}`; // Back Ticks
+
+console.log(nomeCompleto);
+
+//#endregion
+
+//#region Array Operations
+console.log("---------------------------------");
+console.log("Array\n");
+
+
+// Utilizando Colchetes
+let frutas: string[] = ['Abacaxi', 'Laraja', 'Maçã', 'Banana', 'Manga'];
+frutas = frutas.sort();
+
+console.log(frutas);
+console.log();
+
+
+// Utilizando Array Object
+let frutas2: Array<string> = ['Abacaxi', 'Laraja', 'Maçã', 'Banana', 'Manga'];
+
+console.log('Before shuffle ==>\t', frutas2);
+
+function Shuffle(array: any) {
+    for (let i: number = 0; i < array.length; i++) {
+        let swap = array[i];
+        let randomPos: number = Math.floor(Math.random() * array.length);
+        array[i] = array[randomPos];
+        array[randomPos] = swap;
+    }
+
+    return array;
+}
+
+Shuffle(frutas2);
+
+console.log('Shuffled ==>\t\t', frutas2);
+
+//#endregion
+
+//#region Tuples
+console.log("---------------------------------");
+console.log("Tuplas\n");
+
+// Tupla simples
+console.log("Tupla simples");
+
+let pessoa: [string, string, number]; // Nome, cargo, idade
+pessoa = ['Glaucia Lemos', 'Cloud Advocate JavaScript', 34];
+
+console.log(pessoa, '\n');
+
+// Tupla com Spread Operator
+console.log("Tupla com Spread Operator");
+
+let listaFrutas: [string, ...string[]] ;
+listaFrutas = ['Abacaxi', 'Banana', 'Goiaba', 'Pera', 'Melancia'];
+
+console.log(...listaFrutas, '\n');
+
+
+// Tupla com label
+console.log("Tupla com label");
+
+let pessoa2: [nome: string, posicao: string, idade: number];
+pessoa2 = ['Luan Barbosa', 'Programador Junior', 25];
+
+console.log(pessoa2, '\n');
+
+
+
+// Lista Heterogênea de Tupla
+console.log("Lista Heterogênea de Tupla");
+
+let listaFrutas2: [number, boolean, ...string[]] ;
+listaFrutas2 = [5, true, ...listaFrutas];
+
+console.log(listaFrutas2, '\n');
+
+// Função com Tupla
+console.log('Função com Tupla');
+
+function listarPessoas(nomes: string[], idades: number[]) {
+    return [...nomes, ...idades];
+}
+
+let resultado = listarPessoas(['Glaucia', 'Luan'], [34, 24]);
+console.log(resultado, '\n');
+
+// Labeled Tuples com Spread Operator numa função
+console.log('Labeled Tuples com Spread Operator numa função');
+
+type Nome = 
+         | [primeiroNome: string, sobrenome: string]
+         | [primeiroNome: string, nomeDoMeio: string, sobrenome: string];
+
+function criarPessoa (...nome: Nome) {
+    return [...nome];
+}
+
+console.log(criarPessoa('Luan', 'Barbosa'));
+console.log(criarPessoa('Glaucia', 'de Souza', 'Lemos'));
+
+
+//#endregion
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Repositório de acompanhamento do curso de TypeScript
+
+> Criado para acompanhar o curso da Glaucia Lemos sobre o aprendizado de TypeScript
+
+---
+
+
+## 1. Sobre o código
+
+Apesar de seguir um tutorial, muita coisa foi alterada do código original, por motivos de aprendizagem. Quando eu via alguma função que poderia ser divertida de implementar ou uma ideia diferente, além de copiar o curso, o modificava.
+
+## 2. Como rodar o projeto
+
+1. No repositório raiz, execute o seguinte comando:
+`npm install`
+
+2. Escolha o módulo a ser executado, entre no diretório correspondente e execute o comando:
+`node nomeDoArquivo.js`
+
+3. A resposta deve aparecer no console em que o comando foi executado
+
+\
+\
+@Author: Luan Barbosa
